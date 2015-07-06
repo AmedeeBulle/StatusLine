c48bd0f68ac3790fff3474b073759bd8c36059cf
Philippe Vanhaesendonck <AmedeeBulle@users.noreply.github.com>
2015-07-06 19:02:47 +0200

    Save last message for new browser session

--- status_line/__init__.py
+++ status_line/__init__.py
@@ -1,30 +1,47 @@
 # coding=utf-8
 from __future__ import absolute_import
 
+import flask
+
 import octoprint.plugin
 
 class StatusLinePlugin(octoprint.plugin.TemplatePlugin,
                        octoprint.plugin.AssetPlugin,
+                       octoprint.plugin.SimpleApiPlugin,
                        octoprint.plugin.OctoPrintPlugin
                        ):
 
     def __init__(self):
-        pass
+        self.message = ""
+
+    # OctoPrintPlugin hook
 
     def hook_m117(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
         if gcode and gcode == "M117":
             self._logger.debug("Sent M117 command: {0}".format(cmd))
-            self._plugin_manager.send_plugin_message(self._identifier, dict(status_line=cmd[5:]))
+            self.message = cmd[5:]
+            self._plugin_manager.send_plugin_message(self._identifier, dict(status_line=self.message))
+
+    # AssetPlugin
 
     def get_assets(self):
         return {
             "js": ["js/status_line.js"]
         }
 
+    # TemplatePlugin
+
     def get_template_configs(self):
         return [
             dict(type="sidebar", name="Status line", icon="print")
         ]
+
+    # SimpleApiPlugin
+
+    def on_api_get(self, request):
+        return flask.jsonify(dict(
+            status_line=self.message
+        ))
 
 __plugin_name__ = "Status Line"
 
--- status_line/static/js/status_line.js
+++ status_line/static/js/status_line.js
@@ -5,6 +5,21 @@ $(function() {
         self.status_line = ko.observable();
         self.show_status = ko.observable(false);
 
+        self.initialMessage = function(data) {
+            self.status_line(data.status_line);
+            self.show_status(data.status_line ? true : false);
+        };
+
+        self.onStartupComplete = function() {
+            // WebApp started, get message if any
+            $.ajax({
+                url: API_BASEURL + "plugin/status_line",
+                type: "GET",
+                dataType: "json",
+                success: self.initialMessage
+            });
+        }
+
         self.onDataUpdaterPluginMessage = function(plugin, data) {
             if (plugin != "status_line") {
                 return;
